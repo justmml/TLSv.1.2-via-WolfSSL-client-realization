f09b4b584da59392cb3e3eed7eeaec20f453fa1c
justmml <justmml@inbox.ru>
2016-07-07 22:04:55 +0300

    Initial Commit

---
+++ TestCliServWolfSSL/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TestCliServWolfSSL
+//
+//  Created by Mac on 07.07.16.
+//  Copyright © 2016 justmml. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
